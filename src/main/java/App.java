db861c31da3706d74f1aa1ea866a14d865c2ab53
RADI <ameraradi@gmail.com>
2020-06-22 16:51:53 -0400

    iniitial commit

---
+++ src/main/java/App.java
@@ -0,0 +1,13 @@
+public class App {
+    public App() {
+    }
+
+    public String getGreeting() {
+        return "Hello World";
+    }
+
+    public static void main(String[] args)
+    {
+        System.out.println((new App()).getGreeting());
+    }
+}
